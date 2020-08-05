56bb5eed47614da1b4c6b23900047fc9fc224c09
hugom <hugo.marcondes@ifsc.edu.br>
2020-08-05 12:38:19 -0300

    refatoração das classes para demonstrar o conceito de polimorfismo.

--- Heranca/src/Heranca.cpp
+++ Heranca/src/Heranca.cpp
@@ -13,53 +13,65 @@ using namespace std;
 // Base class Shape
 class Shape {
    public:
-	  Shape(int w, int h): _width(w), _height(h) {}
-      int width() {
-         return _width;
-      }
-      int height() {
-         return _height;
-      }
-   private:
-      int _width;
-      int _height;
-};
-
-// Base class PaintCost
-class PaintCost {
-   public:
-      int getCost(int area) {
-         return area * 70;
-      }
+		virtual double area() {
+			cout << "Shape::area() called!" << endl;
+			return 0;
+		}
+		virtual double cost() {
+			cout << "Shape::cost() called!" << endl;
+			return 10;
+		}
 };
 
 // Derived class
-class Rectangle: public Shape, public PaintCost {
+class Rectangle: public Shape {
    public:
-	  Rectangle(int w, int h): Shape(w,h) {
-	      cout << "(" << this << ") Rectangle Constructed!" << endl;
+	  Rectangle(double w, double h): _width(w), _height(h) {
+	      cout << "(" << this << ") Rectangle("<< _width << "," << _height << ") Constructed!" << endl;
 	  }
-      int getArea() {
-         return width() * height();
+
+      double area() {
+         return _width * _height;
       }
+
+      double cost() {
+    	  return area() * 70;
+      }
+
+      double width() { return _width; }
+      double height() { return _height; }
+
       ~Rectangle(){
-    	  cout << "(" << this << ") Rectangle Destroyed!" << endl;
+    	  cout << "(" << this << ") Rectangle("<< _width << "," << _height << ") Destroyed!" << endl;
       }
+   private:
+      double _width;
+      double _height;
 };
 
 int main(void) {
-   Rectangle Rect(5,7);
-   int area;
+   Rectangle obj(5,7);
 
-   cout << "Rectangle (" << Rect.width() << "," << Rect.height() << ")" << endl;
+   cout << "Rectangle (" << obj.width() << "," << obj.height() << ")" << endl;
 
-   area = Rect.getArea();
    // Print the area of the object.
-   cout << "Total area: " << Rect.getArea() << endl;
+   cout << "Total area: " << obj.area() << endl;
 
    // Print the total cost of painting
    cout << "Total paint cost: $";
-   cout << Rect.getCost(area) << endl;
+   cout << obj.cost() << endl;
+
+   Shape * ptr = &obj;
+
+   cout << "Accessing Rectangle obj through Shape ptr." << endl;
+
+   // Print the area of the object.
+   cout << "Total area: " << ptr->area() << endl;
+
+   // Print the total cost of painting
+   cout << "Total paint cost: $";
+   cout << ptr->cost() << endl;
+
 
    return 0;
 }
